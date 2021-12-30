2d56d52f5cdd7ef73c7a87d7e4a61092cef2c861
Joao Conceicao <joaogabrieloliveira81@gmail.com>
2021-12-30 18:22:24 -0300

    Improve Vector2D, methods addeds: div, magnitude and normalize

--- classes/Utils.js
+++ classes/Utils.js
@@ -1,9 +1,45 @@
+/** Bidimensional vector */
 class Vector2D {
     x; y;
 
+    constructor(x,y){
+        this.x = x;
+        this.y = y;
+    }
+
     add(vector){
-        this.x += vector.y;
-        this.x += vector.y;
+        if(vector.x && vector.y){
+            this.x += vector.x;
+            this.x += vector.y;
+        }
+        else{
+            this.x += vector;
+            this.y += vector;
+        }
+        return this;
+    }
+
+    div(vector){
+        if(vector.x && vector.y){
+            this.x /= vector.x;
+            this.x /= vector.y;
+        }
+        else{
+            this.x /= vector;
+            this.y /= vector;
+        }
+        return this;
+    }
+
+    /** Get length between points X and Y*/
+    magnitude(){
+        return Math.sqrt( Math.pow(this.x) + Math.pow(this.y));
+    }
+
+    /** Normalize length to 1 */
+    normalize(){
+        let length = this.magnitude();
+        return length > 0 ? this.div(m) : 0;
     }
 }
 
