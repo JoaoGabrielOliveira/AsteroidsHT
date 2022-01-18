995b105796c790865281971d3bbf2bfc1a919dc2
Joao Conceicao <joaogabrieloliveira81@gmail.com>
2022-01-18 11:15:38 -0300

    Implemeted Transform class to SceneObject classes

--- classes/SceneObject.js
+++ classes/SceneObject.js
@@ -1,24 +1,42 @@
 class SceneObject {
     width; height;
-    position; angle = 0;
+    /** @type {Vector2D} */
+    velocity;
+
+    /** @type {Transform} */
+    transform;
+
+
+    /** Construct a SceneObject
+     * 
+     * @param {Vector2D} position 
+     * @param {Number} width 
+     * @param {Number} height 
+     */
 
     constructor(position, width, height){
-        this.position = position;
+        this.transform = new Transform(position)
+        this.transform.position = position;
+        this.velocity = new Vector2D(0,0);
 
         this.width = width;
         this.height = height;
     }
 
+    /**
+     * 
+     * @param {CanvasRenderingContext2D} context 
+     */
     draw(context) {
-        context.setTransform(this.width,0,0,this.height,this.position.x,this.position.y);
+        let deltaX = this.transform.position.x * Math.cos(this.getRadiansAngle()),
+            deltaY = this.transform.position.y * Math.sin(this.getRadiansAngle());
+        
+        context.setTransform(this.width,0,0,this.height,deltaX,deltaY);
         context.rotate(this.angle);
         context.fillRect(-0.5,-0.5, 1, 1);
     }
 
-    setPosition(x, y){
-        if(x)
-            this.position.x = x;
-        if(y)
-            this.position.y = y;
+    getRadiansAngle(){
+        return (Math.PI / 180) * this.angle;
     }
 }
--- classes/SceneObjectRotable.js
+++ classes/SceneObjectRotable.js
@@ -9,13 +9,14 @@ class SceneObjectRotable extends SceneObject {
     }
 
     draw(context) {
-        context.setTransform(1,0,0,1,this.position.x,this.position.y);
+        context.setTransform(1,0,0,1,this.transform.position.x,this.transform.position.y);
         this.mesh.draw(context);
         context.fill();
     }
 
     lookAt(vector){
-        var deltaY = this.position.y - vector.y, deltaX = this.position.x - vector.x;
+        var deltaY = this.transform.position.y - vector.y,
+            deltaX = this.transform.position.x - vector.x;
         this.angle = Math.atan2(deltaY,deltaX);
     }
 }
