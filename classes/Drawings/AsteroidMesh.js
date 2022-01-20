44e797ffa48c0db34f4df3d9c19aeef8d87993cf
Joao Conceicao <joaogabrieloliveira81@gmail.com>
2022-01-20 16:36:38 -0300

    Created Asteroid Mesh class

---
+++ classes/Drawings/AsteroidMesh.js
@@ -0,0 +1,17 @@
+class AsteroidMesh extends Mesh {
+    width;
+    height;
+
+    constructor(width, height){
+        super();
+        this.width = width;
+        this.height = height;
+
+        this.path2D = new Path2D();
+        this.path2D.moveTo(-0.5 * this.width, -0.5 * this.height);
+        this.path2D.lineTo(0.5  * this.width, -0.5 * this.height);
+        this.path2D.lineTo(0.5  * this.width,  0.5 * this.height);
+        this.path2D.lineTo(-0.5 * this.width,  0.5 * this.height);
+        this.path2D.closePath();
+    }
+}
