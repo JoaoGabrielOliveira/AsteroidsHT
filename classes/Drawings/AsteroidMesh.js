9f42d5d58d88592057b1748efcfb5e62ff8570d2
Joao Conceicao <joaogabrieloliveira81@gmail.com>
2022-03-14 14:16:24 -0300

    Creating the AsteroidMesh

--- classes/Drawings/AsteroidMesh.js
+++ classes/Drawings/AsteroidMesh.js
@@ -8,10 +8,17 @@ class AsteroidMesh extends Mesh {
         this.height = height;
 
         this.path2D = new Path2D();
-        this.path2D.moveTo(-0.5 * this.width, -0.5 * this.height);
-        this.path2D.lineTo(0.5  * this.width, -0.5 * this.height);
-        this.path2D.lineTo(0.5  * this.width,  0.5 * this.height);
-        this.path2D.lineTo(-0.5 * this.width,  0.5 * this.height);
+        this.path2D.moveTo(-0.4, -0.4);
+        this.path2D.lineTo(0, -0.2);
+        this.path2D.lineTo(0.4, -0.3);
+
+        this.path2D.lineTo(0.4,  0);
+        this.path2D.lineTo(0.3, 0.3);
+        this.path2D.lineTo(0.1, 0.4);
+
+        this.path2D.lineTo(0,  0.4);
+        this.path2D.lineTo(-0.4,  0);
+        this.path2D.lineTo(-0.5,-0.2);
         this.path2D.closePath();
     }
 }
